7f25f2f3a0b830b8d63abe28ba2cb3f02ff0b209
kumarin <kumarin@lenze.com>
2021-08-20 11:23:01 +0530

    Add project files.

---
+++ Assignment2/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.IO;
+
+namespace Assignment2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the number for which you want to find pair: ");
+            var input = Console.ReadLine();
+
+            var path = @"..\..\..\..\number.txt";
+
+            var numbers = File.ReadAllText(path).Split(", ");
+
+            var count = 0;
+            foreach (var n in numbers)
+            {
+                if (n == input)
+                    count++;
+            }
+
+            var result = count / 2;
+            Console.WriteLine("Pair of {0} is {1}", input, result);
+        }
+    }
+}
